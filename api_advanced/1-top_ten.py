ad775ae2033a43acf8273d90469b0d82d450adec
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-07-04 19:20:57 +0200

    subreddit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
-"""Script that fetches 10 hot posts for a given subreddit."""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Return 10 hot posts for a given subreddit.
-    If the subreddit is invalid or there are no posts, print None."""
+    """Return 10 hot posts for given subbreddit.
+    if not return 0."""
 
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    subreddit_url =f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+    response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
-        posts = json_data.get('data', {}).get('children', [])
-        if not posts:
-            print(None)
-            return
-        
-        for post in posts:
-            print(post.get('data', {}).get('title'))
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
         print(None)
-
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
