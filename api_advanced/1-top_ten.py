192f04afecb2d0349d018a35cfd0ac4b67cd46e1
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-07-13 01:19:51 +0200

    edited top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-""" Get the titles of the first 10 hot posts for a given subreddit."""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
 
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    try:
-        response = requests.get(subreddit_url, headers=headers,allow_redirects=False)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print(None)
-        return
-    
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+
     if response.status_code == 200:
-        try:
-            json_data = response.json()
-            posts = json_data.get('data').get('children', [])
-            if not posts:
-                print(None)
-                return
-            for post in posts[:10]:
-                print(post.get('data', {}).get('title'))
-        except (ValueError, KeyError):
-            print(None)
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
         print(None)
